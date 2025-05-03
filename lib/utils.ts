4dcc34012f877d5bf1ac515232a71f6b80ca28e9
daaniissh <pkdanish329@gmail.com>
2025-05-03 23:33:11 +0530

    refactor(#utils): improve and clean utility functions

--- lib/utils.ts
+++ lib/utils.ts
@@ -13,3 +13,7 @@ export function formatData( date: string ) {
       year: 'numeric'
   });
 }
+
+export function parseServerActionResponse<T>(response:T){
+  return JSON.parse(JSON.stringify(response));
+}
